48983fbdac79465cb2d0039bcb69d16a7a23de63
Ryota Kinukawa <kinukawa@tombo-works.com>
2023-11-14 11:58:13 +0900

    Add Zynq FSBL package

---
+++ external.mk
@@ -0,0 +1,1 @@
+include $(sort $(wildcard $(BR2_EXTERNAL_ZYBO_Z7_10_PATH)/package/*/*.mk))
---
+++ package/zynq_fsbl/zynq_fsbl.mk
@@ -0,0 +1,24 @@
+################################################################################
+#
+# zynq_fsbl
+#
+################################################################################
+
+ZYNQ_FSBL_VERSION = xlnx_rel_v2022.1
+ZYNQ_FSBL_SITE = $(call github,Xilinx,embeddedsw,$(ZYNQ_FSBL_VERSION))
+ZYNQ_FSBL_LICENSE = GPL-2.0+ MIT
+ZYNQ_FSBL_LICENSE_FILES = license.txt
+ZYNQ_FSBL_DEPENDENCIES += host-arm-gnu-toolchain
+ZYNQ_FSBL_INSTALL_TARGET = NO
+ZYNQ_FSBL_INSTALL_IMAGES = YES
+
+define ZYNQ_FSBL_BUILD_CMDS
+  $(TARGET_MAKE_ENV) $(MAKE1) -C $(@D)/lib/sw_apps/zynq_fsbl/src
+endef
+
+define ZYNQ_FSBL_INSTALL_IMAGES_CMDS
+  $(INSTALL) -m 0644 -t $(BINARIES_DIR) \
+  $(@D)/lib/sw_apps/zynq_fsbl/src/fsbl.elf
+endef
+
+$(eval $(generic-package))
